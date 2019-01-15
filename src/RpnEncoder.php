1fe0a28685ac341ba438e8a27dda5b40fc80576e
Yurii Tatur <skipper2107@gmail.com>
2019-01-15 17:39:22 +0200

    Create RpnEncoder.php

---
+++ src/RpnEncoder.php
@@ -0,0 +1,77 @@
+<?php
+
+class RpnEncoder
+{
+    /**
+     * @var array
+     */
+    private $stack = [];
+ 
+    public function encode(string $expression)
+    {
+        $result = '';
+        $parts = str_split($expression);
+ 
+        foreach ($parts as $part) {
+            if ($part == '^') {
+                foreach ($this->stack as $key => $item) {
+                    if(in_array($item, ['+', '-', '*', '/'])){
+                        $result .= $item;
+                        unset($this->stack[$key]);
+                    } else {
+                        break;
+                    }
+                }
+                array_unshift($this->stack, $part);
+                continue;
+            }
+ 
+            if (in_array($part, ['*', '/'])) {
+                foreach ($this->stack as $key => $item) {
+                    if(in_array($item, ['+', '-'])){
+                        $result .= $item;
+                        unset($this->stack[$key]);
+                    } else {
+                        break;
+                    }
+                }
+                array_unshift($this->stack, $part);
+                continue;
+            }
+ 
+            if (in_array($part, ['+', '-'])) {
+                array_unshift($this->stack, $part);
+                continue;
+            }
+ 
+            if ($part == '(') {
+                array_unshift($this->stack, $part);
+                continue;
+            }
+ 
+            if ($part == ')') {
+                foreach ($this->stack as $key => $item) {
+                    if ($item != '(') {
+                        $result .= $item;
+                        unset($this->stack[$key]);
+                    } else {
+                        unset($this->stack[$key]);
+                        break;
+                    }
+                }
+                if (in_array(reset($this->stack), [])) {
+                    $result .= array_shift($this->stack);
+                }
+                continue;
+            }
+ 
+            $result .= $part;
+        }
+ 
+        foreach ($this->stack as $item) {
+            $result .= $item;
+        }
+ 
+        return $result;
+    }
+}
